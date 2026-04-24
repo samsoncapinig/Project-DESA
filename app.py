aded7837c7b146ab30a6017825ac5a35239b39ff
samsoncapinig <samson.capinig001@deped.gov.ph>
2026-04-24 17:35:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from reportlab.lib.pagesizes import letter
 # PAGE CONFIG
 # =============================
 st.set_page_config(page_title="SDO Masbate City Project DESA", layout="wide")
-st.title(("logo.png", width=80)"SDO Masbate City Project DESA")
+st.title("logo.png", width=80", "SDO Masbate City Project DESA")
 st.markdown("Strict detection of qualitative column headers and full response listing.")
 
 # =============================
