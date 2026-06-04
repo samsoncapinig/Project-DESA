7cf5fe35f343b5d26e3539d5c829667b8bbf4b2a
samsoncapinig <samson.capinig001@deped.gov.ph>
2026-06-04 20:29:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ st.set_page_config(page_title="SDO Masbate City Project DESA", layout="wide")
 st.image("logo.png", width=1200)
 st.title("SDO Masbate City Project DESA")
 st.markdown("Designed for faster data analysis and interpretation of evaluation results.")
-st.markdown(Upload the Daily Evaluations or the End-of-Program Evaluation to get the results of each. You can upload at once the files to get analysis and recommendations.")
 
 # =============================
 # CONSTANTS
